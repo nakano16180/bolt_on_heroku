79277080cd2399a12bffd2e7a84743a87d3544c8
nakano16180 <nakano16180@gmail.com>
2022-04-11 10:57:10 +0900

    use flask

--- app.py
+++ app.py
@@ -1,63 +1,38 @@
-import os
-# Use the package we installed
+import logging
+
 from slack_bolt import App
 
-# Initializes your app with your bot token and signing secret
-app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-)
+logging.basicConfig(level=logging.DEBUG)
+app = App()
 
-@app.event("app_home_opened")
-def update_home_tab(client, event, logger):
-  try:
-    # views.publish is the method that your app uses to push a view to the Home tab
-    client.views_publish(
-      # the user that opened your app's app home
-      user_id=event["user"],
-      # the view object that appears in the app home
-      view={
-        "type": "home",
-        "callback_id": "home_view",
 
-        # body of the view
-        "blocks": [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    )
+@app.command("/hello-bolt-python-heroku")
+def hello(body, ack):
+    user_id = body["user_id"]
+    ack(f"Hi <@{user_id}>!")
 
-  except Exception as e:
-    logger.error(f"Error opening modal: {e}")
 
-# Start your app
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+
+
+# heroku login
+# heroku create
+# git remote add heroku https://git.heroku.com/xxx.git
+
+# export SLACK_BOT_TOKEN=xxx
+# export SLACK_SIGNING_SECRET=xxx
+# heroku config:set SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN
+# heroku config:set SLACK_SIGNING_SECRET=$SLACK_SIGNING_SECRET
+# git checkout -b main
+# git add .
+# git commit -m'Initial commit for my awesome Slack app'
+# git push heroku main
